435154e8486be67d4b59d3021a54eea21af5c72f
Matt Hitchcock <matthew.hitchcock@microsoft.com>
2017-03-31 06:17:34 -0700

    Added commands to generate resources

---
+++ xDSCResources.ps1
@@ -0,0 +1,12 @@
+﻿$ProductID = New-xDscResourceProperty -Name ProductID -Type String -Attribute Key
+$Vendor = New-xDscResourceProperty -Name Vendor -Type String -Attribute Required
+$Ensure = New-xDscResourceProperty -Name Ensure -Type String -Attribute Write -ValidateSet "Present","Absent"
+
+New-xDscResource -Name xMPIODeviceInstalled -Property $ProductID,$Vendor,$Ensure -path .\MPIO -ModuleName xMPIOManagement
+
+
+$BusType = New-xDscResourceProperty -Name BusType -Type String -Attribute Key
+$Ensure = New-xDscResourceProperty -Name Ensure -Type String -Attribute Write -ValidateSet "Present","Absent"
+
+New-xDscResource -Name xMPIODSMBusClaim -Property $BusType,$Ensure -path .\MPIO -ModuleName xMPIOManagement
+
